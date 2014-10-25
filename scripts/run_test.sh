8aca0a5078f0835d6ca39f9c2af66346e95d5d6b
Cedrik <cedrik@datamonk.com>
2014-10-25 23:47:42 +0200

    run test on native mcl

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 mvn package -DskipTests=true
 mr-mcl io.mat.MatFileLoader -i /mnt/hgfs/aufnahmen/16112012_1.mat -o data -te 2 -hdfs -s 50 -t 60
-mr-mcl mapred.SequenceInputJob -Dsigma.I=0.01 -Dsigma.X=0.25 -i data -o matrix -te 2 -nsub 8 -cm -co -zk
-mr-mcl mcl -i matrix -o clustering -cm -co -zk -n
+mr-mcl mapred.SequenceInputJob -Dsigma.I=0.01 -Dsigma.X=0.25 -i data -o matrix -te 2 -r 2 -nsub 64 -cm -co -zk
+#mr-mcl mcl -i matrix -o clustering -cm -co -zk -n -S 100
+mr-mcl util.AbcOutput -i matrix -o /mnt/hgfs/mcl-im-seg/results/test/matrix.abc
+mcl /mnt/hgfs/mcl-im-seg/results/test/matrix.abc --abc -S 100 -R 100 -te 2 -o /mnt/hgfs/mcl-im-seg/results/test/clustering
