caac905dbd773c011a601ff5c0476f46cd2a9fc3
Cedrik <cedrik@datamonk.com>
2014-11-23 13:13:26 +0100

    correct processing

--- src/main/java/classic/InMemoryTransposeJob.java
+++ src/main/java/classic/InMemoryTransposeJob.java
@@ -5,6 +5,9 @@ package classic;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 import io.writables.MCLMatrixSlice;
 import io.writables.MatrixMeta;
@@ -32,29 +35,57 @@ public class InMemoryTransposeJob extends AbstractMCLJob {
 
 	private final class TransposeRunner<M extends MCLMatrixSlice<M>> {
 		
+		private final SortedMap<Integer,SortedMap<Integer,M>> map = new TreeMap<Integer, SortedMap<Integer,M>>();
+		
 		private void run(Path input, Path output) throws IOException {
-			Reader reader = new Reader(getConf(), Reader.file(input));
+			
+			{
+				Reader reader = new Reader(getConf(), Reader.file(input));			
+				
+				SliceId key = new SliceId();
+				M m = MCLContext.getMatrixSliceInstance(getConf());
+				SliceId id = new SliceId();
+				
+				while(reader.next(key, m)){
+					for(M s : m.getSubBlocks(id)){
+						add(id.get(), key.get(), s.deepCopy());
+					}
+				}
+				
+				reader.close();
+			}
+			
 			Writer writer = SequenceFile.createWriter(getConf(), 
 					Writer.file(output), 
 					Writer.keyClass(SliceId.class),
 					Writer.valueClass(SubBlock.class));
 			
-			SliceId key = new SliceId();
-			M m = MCLContext.getMatrixSliceInstance(getConf());
-			SliceId id = new SliceId();
 			SubBlock<M> subBlock = new SubBlock<M>();
 			subBlock.setConf(getConf(), false);
+			SliceId id = new SliceId();
 			
-			while(reader.next(key, m)){
-				subBlock.id = key.get();
-				for(M s : m.getSubBlocks(id)){
-					subBlock.subBlock = s;
+			for(Entry<Integer,SortedMap<Integer,M>> e1 : map.entrySet()){
+				id.set(e1.getKey());
+				SortedMap<Integer, M> subMap = e1.getValue();
+				for(Entry<Integer,M> e2 : subMap.entrySet()){
+					subBlock.id = e2.getKey();
+					subBlock.subBlock = e2.getValue();
 					writer.append(id, subBlock);
 				}
 			}
 			
-			reader.close();
 			writer.close();
+			map.clear();
+		}
+		
+		private void add(Integer id, Integer block_id, M m){
+			SortedMap<Integer,M> subMap = map.get(id);
+			
+			if(subMap == null){
+				subMap = new TreeMap<Integer, M>();
+				map.put(id, subMap);
+			}
+			subMap.put(block_id, m);
 		}
 	}
 
