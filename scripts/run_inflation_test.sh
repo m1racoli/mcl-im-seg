ff11738eca95517778c9c7c5cc03935dd7fbbc37
Cedrik <cedrik@datamonk.com>
2015-01-23 21:12:58 +0100

    run local tests with 2GB client heap

--- scripts/run_inflation_test.sh
+++ scripts/run_inflation_test.sh
@@ -14,6 +14,7 @@ shift
 
 target="s3n://mcl-tests/exp/$exp"
 log="$target/log.txt"
+export HADOOP_CLIENT_OPTS="-Xmx2G"
 
 mr-mcl bmcl -a -b $bal --change-limit 5.0e-5 --local -n -I $inf -S 100 -i "$data/r3_128" -o "$target" -l "$log" -cm -co -vint -zk $@
 mr-mcl util.StreamedNcut -i "$data/r3_abc/matrix.abc" -o some "$target/clustering"
