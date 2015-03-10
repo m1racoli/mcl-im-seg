50f7168390196511ba56417c3c1e5933819a577a
Cedrik <cedrik@datamonk.com>
2015-03-10 21:51:29 +0100

    tests

--- src/main/java/io/test/CSCSliceTest.java
+++ src/main/java/io/test/CSCSliceTest.java
@@ -14,6 +14,7 @@ import mapred.MCLConfigHelper;
 import mapred.MCLContext;
 import mapred.PrintMatrix;
 
+@Deprecated
 public class CSCSliceTest extends MCLContext {
 
 	public static void main(String[] args) throws IOException {
@@ -31,7 +32,7 @@ public class CSCSliceTest extends MCLContext {
 		float[] val = new float[] {1.0f,0.5f,1.0f,3.0f,2.0f,1,2,3};
 		
 		CSCSlice slice = new CSCSlice(conf);
-		slice.fill(col,row,val);
+		//slice.fill(col,row,val);
 		System.out.println(slice);
 		SliceId id = new SliceId();
 		for(CSCSlice m : rewrite(slice,conf).getSubBlocks(id)){
--- src/test/java/io/writables/nat/TestNativeCSCSlice.java
+++ src/test/java/io/writables/nat/TestNativeCSCSlice.java
@@ -3,80 +3,17 @@
  */
 package io.writables.nat;
 
-import static org.junit.Assert.*;
-
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.IntBuffer;
-import java.util.Random;
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import io.writables.TestMCLMatrixSlice;
 
 /**
  * @author Cedrik
  *
  */
-public class TestNativeCSCSlice {
-	
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
+public class TestNativeCSCSlice extends TestMCLMatrixSlice<NativeCSCSlice>  {
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@After
-	public void tearDown() throws Exception {
-	}
-
-	/**
-	 * Test method for {@link io.writables.nat.NativeCSCSliceHelper#clear(java.nio.ByteBuffer, int)}.
-	 */
-	@Test
-	public void testClear() {
-//		int nsub = 128;
-//		ByteBuffer bb = getBBInstance(nsub);
-//		//TODO
-//		NativeCSCSliceHelper.clear(bb);
-//		IntBuffer ibb = bb.asIntBuffer();
-//		for(int i = 0; i < nsub; i++){
-//			assertEquals("buffer is not 0 at position "+i, 0, ibb.get());
-//		}
-	}
-	
-	private ByteBuffer getBBInstance(int nsub){
-		ByteBuffer bb = ByteBuffer.allocateDirect(nsub * Integer.SIZE);
-		bb.order(ByteOrder.nativeOrder());
-		fillIntBuffer(bb.asIntBuffer(), 3141);
-		return bb;
-	}
-	
-	private static void fillIntBuffer(IntBuffer bb, long seed){
-		final Random rand = new Random(seed);
-		for(int i = 0; i < bb.capacity(); i++){
-			bb.put(i, rand.nextInt());
-		}
+	@Override
+	protected Class<NativeCSCSlice> getSliceClass() {
+		return NativeCSCSlice.class;
 	}
 
 }
