a2a122a15a7c79a616b90e8c8cb758e7b2213548
Cedrik <cedrik@datamonk.com>
2015-04-14 00:29:34 +0200

    writables

---
+++ src/main/java/io/writables/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+/**
+ * all around writable implementations and their companion classes.
+ * 
+ * specially the the CSCSlice as the main implementation of an MCLMatrixSlice ca be found here.
+ * 
+ * @author Cedrik
+ *
+ */
+package io.writables;
