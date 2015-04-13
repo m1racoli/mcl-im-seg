1cb2100f5bdb7da3d902084d3eddf74bbcb04c4f
Cedrik <cedrik@datamonk.com>
2015-04-14 00:11:35 +0200

    heap

--- src/main/java/io/heap/FibonacciHeap.java
+++ src/main/java/io/heap/FibonacciHeap.java
@@ -191,5 +191,91 @@ public final class FibonacciHeap<E extends Comparable<E>> extends AbstractQueue<
 	private static final int maxRank(int size){
 		return (int) Math.floor(Math.log(size)/Math.log((1.0 + Math.sqrt(5.0))/2.0));
 	}
+	
+	/**
+	 * heap item for the heap implementation
+	 * 
+	 * @author Cedrik
+	 *
+	 */
+	static final class HeapItem {
+
+		private final Object data;
+		private HeapItem child = null;
+		private HeapItem left;
+		private HeapItem right;
+		private int degree = 0;
+		
+		public HeapItem(Comparable<?> data, HeapItem nb) {
+			this.data = data;
+			
+			if(nb == null){
+				left = this;
+				right = this;
+			} else {
+				right = nb;
+				left = nb.left;
+				nb.left = this;
+				left.right = this;				
+			}
+		}
+		
+		public int degree() {
+			return degree;
+		}
+
+		/**
+		 * remove node from its siblings
+		 */
+		public void decouple(){
+			left.right = right;
+			right.left = left;
+		}
+		
+		public void link(HeapItem parent){
+			left.right = right;
+			right.left = left;
+			
+			if(parent.child == null){
+				parent.child = this;
+				right = this;
+				left = this;
+			} else {
+				left = parent.child;
+				right = parent.child.right;
+				parent.child.right = this;
+				right.left = this;				
+			}
+		}
+		
+		public boolean hasChild(){
+			return child != null;
+		}
+		
+		public HeapItem child(){
+			return child;
+		}
+		
+		public HeapItem right(){
+			return right;
+		}
+		
+		public void takeDownChildren(){
+			HeapItem min_left = left;
+			HeapItem z_child_left = child.left;
+			left = z_child_left;
+			z_child_left.right = this;
+			child.left = min_left;
+			min_left.right = child;
+		}
+		
+		public Object data(){
+			return data;
+		}
+		
+		public boolean hasSiblings(){
+			return this != right;
+		}
+	}
 
 }
