7486b4187561ab6be73f98c8fe49b86e4f47bbba
Cedrik <cedrik@datamonk.com>
2015-04-14 00:19:56 +0200

    cleanup

--- src/main/java/util/MatrixMaker.java
+++ src/main/java/util/MatrixMaker.java
@@ -3,8 +3,6 @@
  */
 package util;
 
-import io.matrix.MatrixEntry;
-
 import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.awt.image.Raster;
@@ -12,6 +10,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.IdentityHashMap;
 import java.util.Map;
 import java.util.PriorityQueue;
@@ -23,6 +22,7 @@ import model.nb.CocentricPixelNeighborhood;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.util.ToolRunner;
+
 import com.beust.jcommander.Parameter;
 
 /**
@@ -196,6 +196,61 @@ public class MatrixMaker extends AbstractUtil {
 	
 	public static void main(String[] args) throws Exception{
 		System.exit(ToolRunner.run(new MatrixMaker(), args));
+	}
+	
+	private static final class MatrixEntry implements Comparable<MatrixEntry> {
+		public long col;
+		public long row;
+		public float val;
+		
+		public MatrixEntry(){
+			this(0,0,0.0f);
+		}
+		
+		private MatrixEntry(long col, long row, float val){
+			this.col = col;
+			this.row = row;
+			this.val = val;
+		}
+		
+		public static MatrixEntry get(long col, long row, float val) {
+			return new MatrixEntry(col,row,val);
+		}
+		
+		@Override
+		public int compareTo(MatrixEntry o) {
+			int cmp = col == o.col ? 0 : col < o.col ? -1 : 1;
+			if(cmp != 0) return cmp;
+			return row == o.row ? 0 : row < o.row ? -1 : 1;
+		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			if(obj instanceof MatrixEntry){
+				MatrixEntry o = (MatrixEntry) obj;
+				return col == o.col && row == o.row && val == o.val;
+			}
+			return false;
+		}
+		
+		@Override
+		public int hashCode() {
+			return (int) (31 * col + row);
+		}
+		
+		@Override
+		public String toString() {
+			return String.format("[c: %d, r: %d, v: %f]", col,row,val);
+		}
+		
+		public static class ValueComparator implements Comparator<MatrixEntry> {
+
+			@Override
+			public int compare(MatrixEntry o1, MatrixEntry o2) {
+				return o1.val < o2.val ? -1 : o1.val > o2.val ? 1 : 0;
+			}
+			
+		}
 	}
 
 }
